de526b6466f150347b297408d7a7a0cf284fd48a
NARENDRAec <99303609+NARENDRAec@users.noreply.github.com>
2024-08-13 09:21:42 +0530

    Delete java_oops/src directory

--- java_oops/src/java_oops_ipack/IAuthService.java
+++
@@ -1,7 +0,0 @@
-package java_oops_ipack;
-
-public interface IAuthService 
-{
-     public boolean authentication(Integer pin);
-     public boolean reset(Integer pin);
-}
--- java_oops/src/java_oops_ipack/ICUI_Service.java
+++
@@ -1,7 +0,0 @@
-package java_oops_ipack;
-
-public interface ICUI_Service 
-{
-	public void showmenu();
-
-}
